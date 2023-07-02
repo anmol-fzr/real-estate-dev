2502fc84ed9ca3d35eb35b5e343fad732229726c
Anmol <88301047+anmol-fzr@users.noreply.github.com>
2023-07-02 15:05:37 +0530

    modal : width fix

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -30,16 +30,16 @@ export default function Modal({ open, close }) {
             <form onSubmit={handleSubmit(onSubmit)} className="fixed z-50 flex w-full gap-2 p-4 overflow-auto -translate-x-1/2 -translate-y-1/2 rounded-lg top-1/2 left-1/2 ">
 
                 <>
-                    <div className="flex justify-center flex-1 max-w-screen-lg mx-auto my-20 bg-white shadow sm:rounded-lg">
-                        <div className="p-6 lg:w-1/2 xl:w-5/12 sm:p-12">
+                    <div className="flex justify-center flex-1 w-full max-w-screen-sm mx-auto my-20 bg-white shadow sm:rounded-lg">
+                        <div className="w-full p-6 sm:p-12">
                             <div className="flex flex-col items-center">
                                 <h1 className="capitalize h1 hero-title " >Contact us</h1>
                                 <div className="flex-1 w-full mt-6">
-                                    <div className="flex flex-col max-w-xs gap-4 mx-auto ">
+                                    <div className="flex flex-col w-full gap-4 mx-auto ">
                                         {formInputs.map(({ name, type, placeholder }) => (
                                             <Input error={errors[ name ]?.message} {...{ name, type, placeholder, register }} />
                                         ))} 
-                                        <button type='submit' className="text-center btn">Submit</button>
+                                        <button type='submit' className="mt-8 text-center btn">Submit</button>
 
                                     </div>
                                 </div>
