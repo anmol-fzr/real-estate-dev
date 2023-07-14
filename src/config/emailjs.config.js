a6a258119ddab41f761c202503fdb6096d75ae54
Anmol <88301047+anmol-fzr@users.noreply.github.com>
2023-07-14 18:56:05 +0530

    change : mail date time format

--- src/config/emailjs.config.js
+++ src/config/emailjs.config.js
@@ -1,10 +1,11 @@
 export default function config({ name, email, phone }) {
+    const currentTime = new Date().toGMTString().slice(0, 16) + " " + new Date().toLocaleTimeString()
     return {
         SecureToken: import.meta.env.VITE_APP_MAIL_TOKEN,
         From: import.meta.env.VITE_APP_MAIL_FROM,
         To: import.meta.env.VITE_APP_MAIL_TO,
-        Subject: `Lead Generated on Vimaan Vihar at ${new Date().toGMTString().slice(0, 22)} `,
-        Body: `<p style="font-size:150%" >A New Lead Generated on Vimaan Vihar at ${new Date().toGMTString().slice(0, 22)}.
+        Subject: `Lead Generated on Vimaan Vihar at ${currentTime} `,
+        Body: `<p style="font-size:150%" >A New Lead Generated on Vimaan Vihar at ${currentTime}.
         <br />
         name: <strong>${name}</strong>,
         <br />
