3027d14830eef1bab8202978496f5b57a763d556
Anmol <88301047+anmol-fzr@users.noreply.github.com>
2023-07-22 23:33:25 +0530

    fix

--- src/components/Call.jsx
+++ src/components/Call.jsx
@@ -5,7 +5,7 @@ export default function Call() {
         <div className='relative ' >
             <a className='z-[5]' href="tel:+919990633353"   >
                 <img loading='lazy' src={x} className='fixed flex items-center w-12 h-12 p-2 rounded-full bg-secondary/25 bottom-[50vh] right-12' alt="call" />
-                <div className='fixed w-12 h-12 duration-1000 rounded-full bottom-[50vh] bg-secondary  right-12' ></div>
+                <div className='fixed w-12 h-12 duration-1000 rounded-full -z-[1] bottom-[50vh] bg-secondary  right-12' ></div>
             </a>
         </div>
     )
